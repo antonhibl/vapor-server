4f6460462b33fb351323b76f308eba11473c2fa7
Anton Hibl <antonhibl11@gmail.com>
2022-04-09 02:35:28 -0700

    cleaned up templates and basic handlers

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,8 @@ import Leaf
 
 // configures your application
 public func configure(_ app: Application) throws {
+    // HTTP v1.1
+    app.http.server.configuration.supportVersions = [.one]
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
     // Leaf Templating engine
